609e2ac9d35b7e52ea06abd5dfa99d9a058182ea
balojr <77960542+balojr@users.noreply.github.com>
2023-04-25 22:20:51 +0300

    Add modification to user class

--- votingapi/src/main/java/com/martin/votingapi/domain/User.java
+++ votingapi/src/main/java/com/martin/votingapi/domain/User.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -18,10 +19,23 @@ public class User  implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String Username;
+  private String username;
   private String password;
   private String email;
   private UserRole userRole;
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
+  @Column(nullable = false)
+  private String createdBy;
+
+  public User(String username, String password, String email, UserRole userRole, String createdBy, String createdAt) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.userRole = userRole;
+    this.createdBy = createdBy;
+    this.createdAt = LocalDateTime.now();
+  }
   private Boolean locked = false;
   private Boolean enabled = false;
 
