3ae36f8e7953465f82c32249c886038f13017548
balojr <77960542+balojr@users.noreply.github.com>
2023-04-25 22:18:07 +0300

    Add candidate dto

--- votingapi/src/main/java/com/martin/votingapi/dto/CandidateDto.java
+++ votingapi/src/main/java/com/martin/votingapi/dto/CandidateDto.java
@@ -3,9 +3,10 @@ package com.martin.votingapi.dto;
 import com.martin.votingapi.domain.enums.UserRole;
 
 public class CandidateDto {
-  String getName();
-  String getParty();
-  String getImage();
-  String getSlogan();
-  UserRole getUserRole();
+//  Long getId();
+//  String getName();
+//  String getParty();
+//  String getImage();
+//  String getSlogan();
+//  UserRole getUserRole();
 }
