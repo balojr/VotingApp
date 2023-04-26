9e5c60647852a0399433ea5b03e66cbb63d5e5f1
balojr <77960542+balojr@users.noreply.github.com>
2023-04-26 13:50:50 +0300

    Add modified imports

--- votingapi/src/main/java/com/martin/votingapi/domain/UserSession.java
+++ votingapi/src/main/java/com/martin/votingapi/domain/UserSession.java
@@ -1,6 +1,6 @@
 package com.martin.votingapi.domain;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
