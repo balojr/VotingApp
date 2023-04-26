cdc9e29c6d0e7e4939b9f4fc846804881148342e
balojr <77960542+balojr@users.noreply.github.com>
2023-04-26 16:09:17 +0300

    Add login request

---
+++ votingapi/src/main/java/com/martin/votingapi/web/rest/requests/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.martin.votingapi.web.rest.requests;
+
+public class LoginRequest {
+  public String username;
+  public String password;
+
+  public String getUsername() {
+    return username;
+  }
+  public String getPassword() {
+    return password;
+  }
+
+}
